b4a8926cb8b260e120f1e14c7503c7433a10d107
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-09-23 20:46:58 -0400

    Create practice#1.kt

---
+++ Kotlin/practice/practice#1.kt
@@ -0,0 +1,13 @@
+package practice
+
+fun main(args: Array<String>){
+    val x = 5
+    val y = 3
+
+    println("x + y = ${x + y}")
+    println("x -  y = ${x - y}")
+    println("x * y = ${x * y}")
+    println("x / y = ${x / y}")
+    println("x % y = ${x % y}")
+
+}
