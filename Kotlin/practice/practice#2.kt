fa9950d7f11a740acd00cbc288f9a958408e1f46
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-09-25 22:31:15 -0400

    Update practice#2.kt

--- Kotlin/practice/practice#2.kt
+++ Kotlin/practice/practice#2.kt
@@ -8,6 +8,14 @@ fun main(args: Array<String>){
     var internectConnection = true
     var myInternectState = if(internectConnection == true) ConnectionTrue() else ConnectionFalse()
     println("Your internect state: $myInternectState")
+
+    val num1 = (1..20).random()
+    val num2 = (1..20).random()
+    println("Number 1: $num1")
+    println("Number 2: $num2")
+    val maxNum = FindMaxNum(num1, num2)
+    println("Maximum number: $maxNum")
+    
 }
 fun printNumbers(num: Int){
     print("$num ")
@@ -23,3 +31,6 @@ fun ConnectionTrue():String{
 fun ConnectionFalse():String{
     return "Disconnected"
 }
+fun FindMaxNum(n1:Int, n2:Int, n3:Int=0, n4:Int=0, n5:Int=0):Int{
+    val listOfNum = listOf(n1, n2, n3, n4, n5)
+    return listOfNum.max()
