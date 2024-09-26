04fa915023075327b551aa743365e5becfc04f51
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-09-25 22:02:04 -0400

    Update practice#2.kt

--- Kotlin/practice/practice#2.kt
+++ Kotlin/practice/practice#2.kt
@@ -3,6 +3,11 @@ package practice
 fun main(args: Array<String>){
     var number = 10
     printNumbers(number)
+    println()
+
+    var internectConnection = true
+    var myInternectState = if(internectConnection == true) ConnectionTrue() else ConnectionFalse()
+    println("Your internect state: $myInternectState")
 }
 fun printNumbers(num: Int){
     print("$num ")
@@ -11,4 +16,10 @@ fun printNumbers(num: Int){
     }else{
         printNumbers(num -1)
     }
+}
+fun ConnectionTrue():String{
+    return "Connected"
+}
+fun ConnectionFalse():String{
+    return "Disconnected"
 }
