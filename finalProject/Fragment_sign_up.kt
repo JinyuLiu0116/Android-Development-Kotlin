c36405516e17ea836d3d36866133510832deed86
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-11-17 19:51:05 -0500

    Update Fragment_sign_up.kt

--- finalProject/Fragment_sign_up.kt
+++ finalProject/Fragment_sign_up.kt
@@ -1,1 +1,14 @@
+package com.example.finalproject
 
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Fragment_sign_up : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+}
